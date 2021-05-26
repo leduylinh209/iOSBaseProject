ad432c777c6df27c0f5b563121865d27662698a0
Linh Leduy <leduylinh209@gmail.com>
2021-05-26 13:20:38 +0700

    Initial Commit

---
+++ BaseProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BaseProject
+//
+//  Created by linhld on 26/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
